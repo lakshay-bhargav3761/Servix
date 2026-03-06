d8a3235fc9de859554b3ee021b450c8b52eb561c
lakshay-bhargav3761 <lakshaybhargav45@gmail.com>
2026-03-07 01:04:57 +0530

    fix frontend serving

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,13 @@ connectDB()
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "../frontend")))
+app.use(express.static(path.join(__dirname, "..")))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/requests", requestRoutes)
 app.use("/api/sla", require("./routes/slaRoutes"));
 const PORT = process.env.PORT || 5000
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"))
+  res.sendFile(path.join(__dirname, "../index.html"))
 })
 app.listen(PORT, () => console.log("Server running on port " + PORT))
